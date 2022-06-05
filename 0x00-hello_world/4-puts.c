c511022bd0bc987dd7e6cebdfe95660679c6cc54
Olusegun_Light <lightolusegun1@gmail.com>
2022-06-05 07:31:45 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #!/bin/bash
-puts("\"programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
