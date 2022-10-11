322d97d766df210f5d51a6304a21f4a1fb79b81b
Olusegun-Light <lightolusegun1@gmail.com>
2022-10-10 22:01:06 -0700

    object

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,43 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strstr -  a function that locates a substring.
- * @haystack: an input string to search in
- * @needle: an input string to locate into string haystack
- * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ * _strstr - prints the consecutive caracters of s1 that are in s2.
+ * @haystack: source string
+ * @needle: searching string
+ *
+ * Return: new string.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *startn = needle, *starth = haystack;
-
 	while (*haystack)
 	{
-		starth = haystack;
-		needle = startn;
-		while (*haystack == *needle)
+		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		{
+			return (haystack);
+		}
+		else
 		{
 			haystack++;
-			needle++;
 		}
-
-		if (*needle == '\0')
-			return (haystack);
-		haystack = starth + 1;
 	}
-	return (NULL);
+	return (0);
+}
+/**
+ * coincidence - define if the string b is inside a.
+ * @a: source string
+ * @b: string to be searched
+ *
+ * Return: 1 if there is coincidence, otherwise 0.
+ */
+int coincidence(char *a, char *b)
+{
+	while (*b && *b == *a)
+	{
+		b++;
+		a++;
+	}
+
+	if (*b == '\0')
+		return (1);
+	else
+		return (0);
 }
